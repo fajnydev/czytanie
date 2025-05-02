c1e58ae896489a61a50b597f8c8fb8a0f84e9d28
Arkadiusz Wrzos <wrzosdev@gmail.com>
2025-05-02 10:15:15 +0200

    Docs folder

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,1 @@
-module.exports={publicPath:process.env.NODE_ENV==='production'?'/czytanie/':'/',outputDir:'dist'};
+module.exports={publicPath:process.env.NODE_ENV==='production'?'/czytanie/':'/',outputDir:'docs'};
